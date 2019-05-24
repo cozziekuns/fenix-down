0b5d0d83b6904e5036ed2434bc8d02747aac8ab7
Cozy Kuns <otwojoy@gmail.com>
2019-05-24 15:44:34 -0700

    templating done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Chart from 'chart.js';
 import './App.css';
 
+Chart.defaults.global.legend.display = false;
 Chart.defaults.global.tooltips.enabled = false;
 
 class App extends React.Component {
@@ -10,7 +11,7 @@ class App extends React.Component {
     const canvas = this.refs.placementCanvas;
     const ctx = canvas.getContext('2d');
 
-    const chart = new Chart(ctx, {
+    new Chart(ctx, {
       type: 'line',
       data: {
           labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
@@ -52,7 +53,7 @@ class App extends React.Component {
     const canvas = this.refs.scoreCanvas;
     const ctx = canvas.getContext('2d');
 
-    const chart = new Chart(ctx, {
+    new Chart(ctx, {
       type: 'line',
       data: {
           labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
@@ -80,7 +81,7 @@ class App extends React.Component {
     const canvas = this.refs.ratingCanvas;
     const ctx = canvas.getContext('2d');
 
-    const chart = new Chart(ctx, {
+    new Chart(ctx, {
       type: 'line',
       data: {
           labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
@@ -103,8 +104,7 @@ class App extends React.Component {
       }
     });
   }
-
-
+  
   componentDidMount() {
     this.createPlacementGraph();
     this.createScoreGraph();
@@ -121,10 +121,10 @@ class App extends React.Component {
                 liebe
               </li>
               <li className="profile-dan">
-                Dan: <strong>七段</strong>
+                Dan: <strong>七段</strong> <small>(1670 / 2800pt)</small>
               </li>
               <li className="profile-rating">
-                Rating: <strong>2014R</strong>
+                Rating: <strong>R2014</strong>
               </li>
             </ul>
           </div>
@@ -137,17 +137,17 @@ class App extends React.Component {
                 Stable Dan: <strong>8.3段</strong>
               </li>
               <li className="profile-rating">
-                Total Games: <strong>2014R</strong>
+                Total Games: <strong>2257</strong>
               </li>
             </ul>
           </div>
         </div>
         <nav className="content-nav">
           <ul>
-            <li>Overview</li>
-            <li>Detailed Stats</li>
-            <li>Highlights</li>
-            <li>Trends</li>
+            <li className="content-nav-highlight">Overview</li>
+            <li><a href="#">Lifetime Stats</a></li>
+            <li><a href="#">Highlights</a></li>
+            <li><a href="#">Trends</a></li>
           </ul>
         </nav>
         <hr className="nav-bar-separator"></hr>
@@ -174,6 +174,164 @@ class App extends React.Component {
             </canvas>
             <div className="overview-graphs-footer">
               Average: 2136.7
+            </div>
+          </div>
+        </div>
+        <div className="overview-other-container">
+          <div className="overview-match-history">
+            <div className="overview-other-title">
+              Match History
+            </div>
+            <table>
+              <tr>
+                <th>Result</th>
+                <th>Player</th>
+                <th>Dan</th>
+                <th>Rating</th>
+                <th>Seat</th>
+                <th>Score</th>
+                <th></th>
+              </tr>
+              <tr>
+                <td>
+                  <ul>
+                    <li className="overview-match-history-placement">1st</li>
+                    <li><small>+90pt</small></li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li><a href="#">liebe</a></li>
+                    <li><a href="#">zeRo</a></li>
+                    <li><a href="#">bichen</a></li>
+                    <li><a href="#">人生フルゼンツ</a></li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>七段</li>
+                    <li>八段</li>
+                    <li>∞段</li>
+                    <li>天鳳位</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>R2120</li>
+                    <li>R2249</li>
+                    <li>RX000</li>
+                    <li>R2391</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>東</li>
+                    <li>南</li>
+                    <li>西</li>
+                    <li>北</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>38000 (+38.0)</li>
+                    <li>31000 (+38.0)</li>
+                    <li>19000 (+38.0)</li>
+                    <li>12000 (+38.0)</li>
+                  </ul>
+                </td>
+                <td><a href="#">Replay</a></td>
+              </tr>
+              <tr>
+                <td>
+                  <ul>
+                    <li className="overview-match-history-placement">1st</li>
+                    <li><small>+90pt</small></li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li><a href="#">liebe</a></li>
+                    <li><a href="#">zeRo</a></li>
+                    <li><a href="#">bichen</a></li>
+                    <li><a href="#">人生フルゼンツ</a></li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>七段</li>
+                    <li>八段</li>
+                    <li>∞段</li>
+                    <li>天鳳位</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>R2120</li>
+                    <li>R2249</li>
+                    <li>RX000</li>
+                    <li>R2391</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>東</li>
+                    <li>南</li>
+                    <li>西</li>
+                    <li>北</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>38000 (+38.0)</li>
+                    <li>31000 (+16.0)</li>
+                    <li>19000 (-16.0)</li>
+                    <li>12000 (-38.0)</li>
+                  </ul>
+                </td>
+                <td><a href="#">Replay</a></td>
+              </tr>
+            </table>
+          </div>
+          <div className="overview-misc">
+            <div className="overview-other-title">
+              Stats
+            </div>
+            <div className="overview-stats">
+              <table>
+                <tr>
+                  <td>Hand Win Rate:</td>
+                  <td className="overview-highlights-value">21.6%</td>
+                </tr>
+                <tr>
+                  <td>Hand Deal-In Rate:</td>
+                  <td className="overview-highlights-value">10.9%</td>
+                </tr>
+                <tr>
+                  <td>Riichi Rate:</td>
+                  <td className="overview-highlights-value">23.4%</td>
+                </tr>
+                <tr>
+                  <td>Call Rate:</td>
+                  <td className="overview-highlights-value">30.9%</td>
+                </tr>
+              </table>
+            </div>
+            <div className="overview-other-title">
+              Highlights
+            </div>
+            <div className="overview-highlights">
+            <table>
+                <tr>
+                  <td>Biggest Comeback</td>
+                  <td className="overview-highlights-value">45300</td>
+                  <td className="overview-highlights-value"><a href="#">Replay</a></td>
+                </tr>
+                <tr>
+                  <td>Biggest Throw</td>
+                  <td className="overview-highlights-value">-42000</td>
+                  <td className="overview-highlights-value"><a href="#">Replay</a></td>
+                </tr>
+              </table>
             </div>
           </div>
         </div>
